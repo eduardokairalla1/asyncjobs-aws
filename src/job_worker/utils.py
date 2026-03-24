848146701dc800996f587dfe46c311495e7eaec7
Eduardo Kairalla <eduardokairalla123@gmail.com>
2026-03-24 20:45:29 -0300

    Add function to manage job status updates in DynamoDB

--- src/job_worker/utils.py
+++ src/job_worker/utils.py
@@ -3,6 +3,7 @@ Utility functions for the job worker.
 """
 
 # --- IMPORTS ---
+from clients import JOBS_TABLE
 from clients import RESULTS_BUCKET
 from clients import S3
 from datetime import datetime
@@ -15,6 +16,7 @@ import random
 # --- TYPES ---
 from typing import Any
 from typing import Dict
+from typing import Optional
 
 
 # --- CODE ---
@@ -74,3 +76,38 @@ def upload_to_s3(job_id: str, result: Dict[str, Any]) -> str:
 
     # return the presigned URL
     return presigned_url
+
+
+def update_status(job_id: str,
+                  status: str,
+                  result_url: Optional[str] = None) -> None:
+    """
+    Update the status of a job.
+
+    :param job_id: The ID of the job.
+    :param status: The new status of the job.
+    :param result_url: The URL of the result, if applicable.
+
+    :return: None
+    """
+
+    # get the current timestamp for the update
+    now = datetime.now(timezone.utc).isoformat()
+
+    # construct the update expression and attribute values
+    update_expr = 'SET #s = :status, updated_at = :updated_at'
+    expr_names = {'#s': 'status'}
+    expr_values = {':status': status, ':updated_at': now}
+
+    # result URL is provided: include it in the update expression and values
+    if result_url is not None:
+        update_expr += ', result_url = :result_url'
+        expr_values[':result_url'] = result_url
+
+    # update the job item in the DynamoDB
+    JOBS_TABLE.update_item(
+        Key={'job_id': job_id},
+        UpdateExpression=update_expr,
+        ExpressionAttributeNames=expr_names,
+        ExpressionAttributeValues=expr_values,
+    )
